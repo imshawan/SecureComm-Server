f444dba0f3a267dab19996dbe542c0985174e824
Shawan Mandal <shawan.sm049@gmail.com>
2022-08-29 23:57:17 +0530

    Added CORS policy

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,3 +2,4 @@ const middlewares = module.exports;
 
 middlewares.authentication = require('./authentication');
 middlewares.validation = require('./validators');
+middlewares.cors = require('./cors');
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { utilities } = require('../utils');
+const { cors } = require('../middlewares');
 
+router.options('/*',cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 
-router.use('/api/v1/users', require('./user'));
-router.use('/api/v1/auth', require('./auth'));
+router.use('/api/v1/users', cors.cors, require('./user'));
+router.use('/api/v1/auth', cors.cors, require('./auth'));
 
 // 404 handling
 router.use('/', (req, res) => {
