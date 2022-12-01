d13a79ba9309f7af80fdf2446a86b2b5d532770a
Shawan Mandal <shawan.sm049@gmail.com>
2022-12-01 10:24:02 +0530

    Added auth for socket connections

--- socket.io/index.js
+++ socket.io/index.js
@@ -1,4 +1,7 @@
 const { Server } = require('socket.io');
+const passport = require('passport');
+const {ExtractJwt} = require('passport-jwt');
+const {Session} = require('../models');
 
 const Sockets = module.exports;
 
@@ -18,10 +21,35 @@ Sockets.init = function (server) {
         }
 	};
 
+    socketIO.use(authenticate);
     socketIO.on('connection', onConnection);
     
     socketIO.listen(server, sockOptions);
     Sockets.io = socketIO;
+}
+
+async function authenticate (socket, next) {
+    const {headers, request} = socket.handshake;
+    const socketRequest = {...request, headers};
+
+    const authorizationToken = ExtractJwt.fromAuthHeaderAsBearerToken()(socketRequest);
+
+    if (!authorizationToken) {
+        return next(new Error('Authorization token was not found in this connection'));
+    }
+
+    passport.authenticate('jwt', {session: false}, async function (err, user) {
+        if (err || !user) {
+            return next(new Error('Unauthorized! A valid token was not found in this connection'));
+        }
+        
+        let session = await Session.findOne({user: user._id});
+        if (!session || session.token != authorizationToken) {
+            return next(new Error('Unable to verify the token! Please retry logging in'));
+        }
+
+        next();
+    })(socketRequest, {}, next);
 }
 
 function onConnection (socket) {
